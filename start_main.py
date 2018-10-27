720e118f9244a281848605f904098ab7ecd63fc7
Tomi Sepponen <tomi.sepponen@student.tut.fi>
2018-10-27 16:45:50 +0300

    ENH: Reworked program; yaml conf and improved calibration/teaching

--- start_main.py
+++ start_main.py
@@ -1,14 +1,10 @@
 from __future__ import division
-from functools import partial
 import yaml
 import os
 
-from legoassembler import script_build_tools
-from legoassembler.communication import URServer, URClient
-from legoassembler.build import *
+from legoassembler.main import run
 
-
-def load_config():
+def _load_config():
     """ Load configuration from config.yml
 
     Returns
@@ -19,11 +15,11 @@ def load_config():
 
     with open('config.yml', 'r') as f:
         cfg = yaml.load(f)
-    discover_scripts(cfg)
+    _discover_scripts(cfg)
     return cfg
 
 
-def discover_scripts(config, pckg_name='legoassembler'):
+def _discover_scripts(config, pckg_name='legoassembler'):
     """ Construct and validate absolute paths to scripts
 
     Parameters
@@ -47,79 +43,9 @@ def discover_scripts(config, pckg_name='legoassembler'):
             scripts[key] = new_path
 
 
-def upload_script(script, gripper_base_def, gripper_fun_def, ip_ur, port_ur, ip_pc, port_pc):
-
-    client = URClient()
-    client.connect(ip_ur, port_ur)
-
-    script = script_build_tools.build(script, gripper_base_def, gripper_fun_def, ip_pc, port_pc)
-
-    client.send(script)
-
-
 if __name__ == '__main__':
 
-    cfg = load_config()
-    cfg_net = cfg['network']
-    cfg_scripts = cfg['ur_scripts']
-    cfg_calib = cfg['calibration_data']
-    cfg_env = cfg['environment']
+    cfg = _load_config()
+    run(cfg)
 
-    # Setup
-    # -------------------
-
-    serv = URServer(cfg_net['host']['ip'], cfg_net['host']['port'])
-
-    # Preload script building with all but the script name itself
-    load_script = partial(upload_script, gripper_base_def=cfg_scripts['gripper_urcap_preamble'],
-                gripper_fun_def=cfg_scripts['gripper_function_definition'],
-                          ip_ur=cfg_net['ur']['ip'], port_ur=cfg_net['ur']['port'],
-                          ip_pc=cfg_net['host']['ip'], port_pc=cfg_net['host']['port'])
-
-
-    # TODO: connect to cam server (or could be mv's job)
-    # TODO: create mv
-    mv = None
-
-    # Environment calibration
-    # -------------------
-    if input('Load previous environment calibration data [Y/n]: ') == 'Y':
-        with open(cfg_calib['platform']) as f:
-            environment = json.load(f)
-    else:
-        load_script(script=cfg_scripts['calibrate_platform'])
-        serv.accept()
-        environment = paltform_calibration(serv, cfg_env['travel_height'])
-        with open(cfg_calib['platform'], 'w') as outfile:
-            json.dump(environment, outfile)
-        serv.close()
-
-    # Build preview
-    #-------------------
-    if input('Live preview calibration poses [Y/n]: ') == 'Y':
-        load_script(cfg_scripts['build_preview'])
-        serv.accept()
-        build_preview(serv, environment)
-        serv.close()
-
-    # Camera calibration
-    # -------------------
-    if input('Load previous camera calibration data [Y/n]: ') == 'Y':
-        with open(cfg_calib['camera']) as f:
-            camera_calibr = json.load(f)
-    else:
-        load_script(cfg_scripts['calibrate_camera'])
-        serv.accept()
-        camera_calibr = camera_calibration(serv, mv)
-        with open(cfg_calib['camera'], 'w') as outfile:
-            json.dump(camera_calibr, outfile)
-        serv.close()
-
-    # Building
-    # -------------------
-    if input('Start building [Y/n]: ') == 'Y':
-        load_script(cfg_scripts['build'])
-        serv.accept()
-        build(serv, environment, camera_calibr, mv)
-        serv.close()
 
